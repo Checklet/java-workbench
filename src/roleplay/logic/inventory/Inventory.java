fea0500860eca972eeff7b5afd008b80a58f862f
Checklet <ac.cekic@outlook.com>
2026-06-16 23:28:01 +0200

    [Roleplay] Implemented functionalities for `inventory` and `item`.

--- src/roleplay/logic/inventory/Inventory.java
+++ src/roleplay/logic/inventory/Inventory.java
@@ -1,8 +1,51 @@
 package roleplay.logic.inventory;
 
+import java.io.Serial;
 import java.io.Serializable;
 import java.util.HashSet;
 
 public class Inventory extends HashSet<Item> implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 2010L;
 
+    private int capacity;
+
+    public Inventory(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public Inventory() {
+        this(60);
+    }
+
+    @Override
+    public boolean add(Item item) {
+        if (!exceedsCapacity(item.getWeight()))
+            return super.add(item);
+
+        return false;
+    }
+
+    public int getWeight() {
+        int weight = 0;
+
+        for (Item item : this)
+            weight += item.getWeight();
+
+        return weight;
+    }
+
+    public boolean exceedsCapacity(int weight) {
+        return getWeight() + weight > capacity;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        if (!(o instanceof Item i)) return false;
+
+        for (Item item : this)
+            if (i.getName().equals(item.getName())) return true;
+
+        return false;
+    }
 }
--- src/roleplay/logic/inventory/Item.java
+++ src/roleplay/logic/inventory/Item.java
@@ -1,4 +1,41 @@
 package roleplay.logic.inventory;
 
-public class Item {
+import java.io.Serial;
+import java.io.Serializable;
+
+public class Item implements Serializable, Comparable<Item> {
+    @Serial
+    private static final long serialVersionUID = 2020L;
+
+    private String name;
+    private int weight;
+
+    public Item(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    @Override
+    public int compareTo(Item item) {
+        char[] thisName = name.toCharArray();
+        char[] otherName = item.name.toCharArray();
+
+        for (int i = 0; i < thisName.length && i < otherName.length; i++) {
+            if (thisName[i] > otherName[i])
+                return -1;
+
+            else if (thisName[i] < otherName[i])
+                return 1;
+        }
+
+        return Integer.compare(otherName.length, thisName.length);
+    }
 }
